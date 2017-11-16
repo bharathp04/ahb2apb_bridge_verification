e1f9bb57a18439c1158d6152fe7b32c1c677f421
Bharath Tupaki <bharathp04@gmail.com>
2017-11-15 23:38:21 -0800

    Updated uvm_sequence_item with more constraints

--- hvl/ahb_master_transaction.svh
+++ hvl/ahb_master_transaction.svh
@@ -4,16 +4,59 @@ class ahb_master_transaction extends uvm_sequence_item;
 	`uvm_object_utils(ahb_master_transaction)
 	
 	logic HSEL;
-	rand logic [1:0]HTRANS;
-	rand logic [HADDR_SIZE-1:0]HADDR;
-	rand logic [HDATA_SIZE-1:0]HWDATA;
-	rand logic HWRITE;
-	rand logic [2:0]HSIZE;
-	rand logic [2:0]HBURST;
+	rand trans_type_t HTRANS[];
+	rand logic [HADDR_SIZE-1:0]HADDR[];
+	rand logic [HDATA_SIZE-1:0]HWDATA[];
+	rand rw_t HWRITE;
+	rand size_t HSIZE;
+	rand burst_type_t HBURST;
 	rand logic [3:0]HPROT;
 	rand logic HMASTLOCK;
 	
-	//write constraints here
+	constraint addr_size{
+		if(HBURST == SINGLE)
+			HADDR.size == 1;
+						
+		if(HBURST == INCR)
+			HADDR.size < (1024/2^HSIZE);
+			
+		if(HBURST inside {WRAP4, INCR4}) 
+			HADDR.size == 4;
+			
+		if(HBURST inside {WRAP8, INCR8})
+			HADDR.size == 8;
+			
+		if(HBURST inside {WRAP16, INCR16})
+			HADDR.size == 16;
+			
+		HADDR.size > 0;
+	}
+	
+	//Add kb_boundary constraint
+	
+	constraint addr_boundary{
+		if(HSIZE == HALFWORD)
+			foreach(HADDR[i])
+				HADDR[i][0] == 0;
+		
+		if(HSIZE == WORD)
+			foreach(HADDR[i])
+				HADDR[i][1:0] == 0;
+	}
+	
+	constraint addr_value_incr{
+		if(HBURST != SINGLE){
+			if(HBURST inside {INCR, INCR4, INCR8, INCR16}){
+				foreach(HADDR[i]){
+					if(i != 0){
+						HADDR[i] == HADDR[i-1] + 2**HSIZE;
+					}
+				}
+			}
+		}
+	}
+	
+	
 	
 	function new(string name= "");
 		super.new(name);
